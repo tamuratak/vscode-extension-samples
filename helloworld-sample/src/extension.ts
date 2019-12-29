50e6b7fddf425529453ba644b1d7c6a178730b6f
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-12-29 09:11:12 +0900

    edit

--- helloworld-sample/src/extension.ts
+++ helloworld-sample/src/extension.ts
@@ -3,36 +3,33 @@ import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
     const rootUrl = vscode.Uri.file(context.extensionPath)
-    const disposable = vscode.commands.registerCommand('extension.helloWorld', async (args, brgs, crgs) => {
+    let inset: vscode.WebviewEditorInset
+    let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
         if (!vscode.window.activeTextEditor) {
             return;
         }
 
-        const inset = vscode.window.createWebviewTextEditorInset(
-            vscode.window.activeTextEditor, 5, 30,
+        inset = vscode.window.createWebviewTextEditorInset(
+            vscode.window.activeTextEditor, 5, 10,
             { localResourceRoots: [ rootUrl ] }
             );
         inset.onDidDispose(() => {
             console.log('WEBVIEW disposed...');
         });
         inset.webview.html = getHtml(rootUrl);
-        setTimeout(() => {
-            inset.updateHeight(3)
-        }, 5000)
 
-        /* the following works well.
-        const panel = vscode.window.createWebviewPanel(
-            'catCoding',
-            'Cat Coding',
-            vscode.ViewColumn.One,
-            {
-              localResourceRoots: [ rootUrl ]
-            }
-          );
-        panel.webview.html = getHtml(rootUrl); 
-        */
     });
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
+
+    disposable = vscode.commands.registerCommand('extension.increaseHeight', () => {
+        inset && inset.updateHeight(inset.height+5)
+    })
+    context.subscriptions.push(disposable);
+
+    disposable = vscode.commands.registerCommand('extension.decreaseHeight', () => {
+        inset && inset.updateHeight(inset.height-5)
+    })
+    context.subscriptions.push(disposable);
 }
 
 function getHtml(rootUrl: vscode.Uri) {
