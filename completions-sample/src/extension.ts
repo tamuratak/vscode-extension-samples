b515873552f5005a1d42fbea5bd813d901cad253
Takashi Tamura <tamuratak@users.noreply.github.com>
2020-10-03 09:18:14 +0900

    edit

--- completions-sample/src/extension.ts
+++ completions-sample/src/extension.ts
@@ -6,7 +6,8 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	const provider1 = vscode.languages.registerCompletionItemProvider('plaintext', {
+	console.log('actv')
+	const provider1 = vscode.languages.registerCompletionItemProvider({ scheme: 'file', language: 'abc' }, {
 
 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
 
@@ -47,7 +48,7 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	const provider2 = vscode.languages.registerCompletionItemProvider(
-		'plaintext',
+		{ scheme: 'file', language: 'abc' },
 		{
 			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
 
