bf38f4eac0901dacb16d463b9735ea29faa37ea3
Takashi Tamura <tamuratak@users.noreply.github.com>
2020-10-22 18:11:41 +0900

    edit

--- helloworld-sample/src/extension.ts
+++ helloworld-sample/src/extension.ts
@@ -1,23 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const htmlString = `
+<script>
+// Should work on macOS.
+const ev0 = {"altKey":false,"code":"KeyN","ctrlKey":false,"isComposing":false,"key":"n","location":0,"metaKey":true,"repeat":false,"shiftKey":false};
+setTimeout(() => window.dispatchEvent(new KeyboardEvent('keydown', ev0)), 2000);
+
+// Should work on windows.
+const ev1 = {"altKey":false,"code":"KeyN","ctrlKey":true,"isComposing":false,"key":"n","location":0,"metaKey":false,"repeat":false,"shiftKey":false};
+setTimeout(() => window.dispatchEvent(new KeyboardEvent('keydown', ev1)), 3000);
+</script>
+`
+
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "helloworld-sample" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
+		const panel = vscode.window.createWebviewPanel(
+			'aaa',
+			'Cat Coding',
+			vscode.ViewColumn.One,
+			{
+					enableScripts: true,
+			}
+		)
+		panel.webview.html = htmlString
+	})
 	context.subscriptions.push(disposable);
 }
