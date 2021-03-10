29f000199fc9804630118254066c6ca2373d7135
Takashi Tamura <tamuratak@users.noreply.github.com>
2021-03-11 08:29:17 +0900

    edit

--- helloworld-sample/src/extension.ts
+++ helloworld-sample/src/extension.ts
@@ -1,23 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld-sample" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+  const rootUrl = vscode.Uri.file(context.extensionPath)
+  const disposable = vscode.commands.registerCommand('extension.helloWorld', async () => {
+    if (!vscode.window.activeTextEditor) {
+      return;
+    }
+    
+    const panel = vscode.window.createWebviewPanel(
+      'catCoding',
+      'Cat Coding',
+      { viewColumn: vscode.ViewColumn.Beside },
+      { enableScripts: true, localResourceRoots: [] }
+    );
+    panel.webview.html = getHtml();
+  });
+  context.subscriptions.push(disposable);
 }
+
+function getHtml() {
+	return `
+<!DOCTYPE html><html><head>
+
+</head>
+<img src="https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/tiger.svg" style="width: 100%">
+`
+}
+// this method is called when your extension is deactivated
+export function deactivate() {}
