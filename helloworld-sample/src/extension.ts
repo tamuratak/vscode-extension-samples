378fd9f56a95bf616e6d9f5dffb3016b07da4338
Takashi Tamura <tamuratak@users.noreply.github.com>
2021-03-11 09:14:03 +0900

    edit

--- helloworld-sample/src/extension.ts
+++ helloworld-sample/src/extension.ts
@@ -1,23 +1,28 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "helloworld-sample" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+	context.subscriptions.push(
+		vscode.languages.registerHoverProvider({ scheme: '*' }, new HoverProvider(context.extensionPath))
+	)
 	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World!');
 	});
+}
 
-	context.subscriptions.push(disposable);
+class HoverProvider implements vscode.HoverProvider {
+	path: string
+	constructor(path: string) {
+		this.path = path
+	}
+	provideHover(): vscode.Hover {
+		const insideExtensionUri = vscode.Uri.file(path.join(this.path, 'tiger.svg'))
+		const fileUri = vscode.Uri.file('/Users/tamura/Downloads/tiger.svg')
+		const serverUri = vscode.Uri.parse('https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/tiger.svg')
+		const md = `ext: ![svgimg](${insideExtensionUri.toString(true)}) local: ![svgimg](${fileUri.toString(true)}), server: ![svgimg](${serverUri.toString(true)})`
+		const mdString = new vscode.MarkdownString(md)
+		const hover = new vscode.Hover(mdString)
+		return hover
+	}
 }
