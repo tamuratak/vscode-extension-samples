a25a565d5b1c6b11ad431dd0d4efc3fb2309693a
Takashi Tamura <tamuratak@users.noreply.github.com>
2021-11-22 12:55:57 +0900

    edit

--- completions-sample/src/extension.ts
+++ completions-sample/src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 			const snippetCompletion = new vscode.CompletionItem('Good part of the day');
 			snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
-			const md = new vscode.MarkdownString(`1 <img src="${pngPathUriString}"> 2 <img src="${pngPath}"> 3 ![a](${pngPathUriString}) 4 ![a](${pngPath})`);
+			const md = new vscode.MarkdownString(`1 <img src="${pngPathUriString}">`);
 			md.supportHtml = true
 			snippetCompletion.documentation = md
 
