53ccfff2cb3a0cf8fda62018af47ae0e960839a3
Takashi Tamura <tamuratak@users.noreply.github.com>
2023-03-19 11:27:57 +0900

    edit

--- completions-sample/src/extension.ts
+++ completions-sample/src/extension.ts
@@ -9,11 +9,8 @@ export function activate(context: vscode.ExtensionContext) {
 	const provider1 = vscode.languages.registerCompletionItemProvider('plaintext', {
 
 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-
-			// a simple completion item which inserts `Hello World!`
 			const item01 = new vscode.CompletionItem('aaa');
 			item01.additionalTextEdits = [vscode.TextEdit.insert(position.translate(0, 10), 'aaa')];
-			// return all completion items as array
 			return [
 				item01
 			];
