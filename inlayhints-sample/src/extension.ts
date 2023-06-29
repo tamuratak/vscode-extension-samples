b9eb5ee759a8d118ddeb559844d91995831f02b5
Takashi Tamura <tamuratak@users.noreply.github.com>
2023-06-29 21:31:30 +0900

    edit

--- inlayhints-sample/src/extension.ts
+++ inlayhints-sample/src/extension.ts
@@ -5,29 +5,29 @@ import * as vscode from 'vscode';
 export class LtInlayHintsProvider implements vscode.InlayHintsProvider {
     private readonly eventEmitter = new vscode.EventEmitter<void>()
     readonly onDidChangeInlayHints: vscode.Event<void>
-	private count = 0
+    private count = 0
 
     constructor() {
         this.onDidChangeInlayHints = this.eventEmitter.event
         setInterval(() => {
-			console.log('firing')
+            console.log('firing')
             this.eventEmitter.fire()
         }, 2000)
     }
 
     async provideInlayHints(document: vscode.TextDocument, range: vscode.Range) {
-		if (this.count === 0) {
-			this.count++
-			console.log('returning empty')
-			return []
-		} else {
-			return [new vscode.InlayHint(new vscode.Position(0, 0), `Hello World (${this.count++})`)]
-		}
-	}
+        if (this.count === 0) {
+            this.count++
+            console.log('returning empty')
+            return []
+        } else {
+            return [new vscode.InlayHint(new vscode.Position(0, 0), `Hello World (${this.count++})`)]
+        }
+    }
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "helloworld-sample" is now active!');
+    console.log('Congratulations, your extension "helloworld-sample" is now active!');
 
-	vscode.languages.registerInlayHintsProvider({ language: 'typescript' }, new LtInlayHintsProvider())
+    vscode.languages.registerInlayHintsProvider({ language: 'typescript' }, new LtInlayHintsProvider())
 }
