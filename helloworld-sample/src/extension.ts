9ee81bb69dadd320fe4d5020a768a995cab1b652
Takashi Tamura <tamuratak@users.noreply.github.com>
2024-06-08 10:37:42 +0900

    edit

--- helloworld-sample/src/extension.ts
+++ helloworld-sample/src/extension.ts
@@ -1,9 +1,6 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const MODEL_SELECTOR: vscode.LanguageModelChatSelector = { vendor: 'copilot', family: 'gpt-3.5-turbo' };
 export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -12,9 +9,25 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	const disposable = vscode.commands.registerCommand('extension.helloWorld', async () => {
+		const [model] = await vscode.lm.selectChatModels(MODEL_SELECTOR);
+		const messages = [
+			vscode.LanguageModelChatMessage.User(`User
+Completion candidates of the following. Up to three candidates. In JSON format.
 
+{
+   "candidates": [{item: <candidate>, reason: <string>}]
+}
+
+Display a message box to the user.
+If the config is `)
+		];
+		const chatResponse = await model.sendRequest(messages, { modelOptions: {"type": "json_object"}});
+		const result = []
+		for await (const fragment of chatResponse.text) {
+			result.push(fragment)
+		}
+		console.log(result.join(''))
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World!');
 	});
