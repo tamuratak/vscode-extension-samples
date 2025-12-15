d33f565fbeb68a426950567bb5544c43efa09a07
Takashi Tamura <tamuratak@users.noreply.github.com>
2025-12-16 07:00:03 +0900

    Refactor chat participant response formatting and remove unused import

--- chat-sample/src/chatUtilsSample.ts
+++ chat-sample/src/chatUtilsSample.ts
@@ -1,5 +1,4 @@
 import * as vscode from 'vscode';
-import * as chatUtils from '@vscode/chat-extension-utils';
 
 export function registerChatLibChatParticipant(context: vscode.ExtensionContext) {
 	const handler: vscode.ChatRequestHandler = async (
@@ -9,7 +8,11 @@ export function registerChatLibChatParticipant(context: vscode.ExtensionContext)
 		token: vscode.CancellationToken
 	) => {
 		await vscode.lm.invokeTool('chat-tools-sample_tabCount', { toolInvocationToken: request.toolInvocationToken, input: { tabGroup: "const a = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;" } }, token)
-		stream.markdown(new vscode.MarkdownString('done!'));
+		stream.markdown(new vscode.MarkdownString(`done!
+~~~ts
+${'const a = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;\n'.repeat(1000)}
+~~~
+`));
 	};
 
 	const chatLibParticipant = vscode.chat.createChatParticipant('chat-tools-sample.catTools', handler);
